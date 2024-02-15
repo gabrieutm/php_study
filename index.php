73cbc226eafb2fff4ab80a25a404b813b402e640
gabrieutm <gtmachado08@gmail.com>
2024-02-15 20:38:32 -0300

    my first php code
    :)

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    echo"Hello World <br>";
+    echo"I'm studying PHP";
+    //this is a comment
+    /*multi
+    line
+    comment
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+</head>
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+</html>
